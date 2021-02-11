31663f3cec179e2c95d94717f72a616ec76d69dc
Jesse <jessedaniel@live.ca>
2021-02-11 02:49:47 +0000

    added function to check if status code not equal 200, throws error

--- fetcher.js
+++ fetcher.js
@@ -7,16 +7,20 @@ const fs = require('fs');
 
 
 const fetcher = function (url, path) {
-  if(url === undefined) {
+  if(url === undefined) { 
     console.log('URL cannot be undefined')
   };
   if(path === undefined) {
     console.log('Please input valid path.')
   };
-  if( url !== undefined && path !== undefined) {
+  if(url !== undefined && path !== undefined) {
   request(url, (error, response, body) => {
     console.log(`error:`, error);
     console.log(`statuscode:`, response && response.statusCode);
+    if(response.statusCode !== 200) {
+      console.log("URL ERROR");
+      process.exit()
+    };
     fs.writeFile(path, body, (err) => {
       if (err) {
         return console.log(err.message);
